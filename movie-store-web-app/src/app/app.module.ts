50eefcb39cb6db92049cb1decce2b710922fbab9
Dusan Janosevic <dusan.janosevic123@gmail.com>
2023-08-01 09:16:16 +0200

    finished upgrading to angular 15

--- movie-store-web-app/src/app/app.module.ts
+++ movie-store-web-app/src/app/app.module.ts
@@ -27,6 +27,25 @@ const isIE =
         HttpClientModule,
         SharedModule,
         ToastrModule.forRoot(),
+        MsalModule.forRoot(
+            new PublicClientApplication({
+                auth: {
+                    clientId: '4e1ff54b-bf34-4f45-83ce-e50fc32967cd',
+                    authority: 'https://login.microsoftonline.com/common',
+                    redirectUri: 'http://localhost:4200',
+                    scopes: [
+                        'api://dbf7f51e-d046-435b-88ee-c4f9ee872967/to-do-lists.read',
+                        'api://dbf7f51e-d046-435b-88ee-c4f9ee872967/to-do-lists.write'
+                    ],
+                },
+                cache: {
+                    cacheLocation: "localStorage",
+                    storeAuthStateInCookie: isIE
+                },
+            }),
+            null,
+            null
+        ),
     ],
     providers: [],
     bootstrap: [AppComponent, MsalRedirectComponent],
